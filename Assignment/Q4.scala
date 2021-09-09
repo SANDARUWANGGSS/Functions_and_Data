af8ea3dc1badf0e380111b286438df677ee03fe4
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2021-09-09 22:13:10 +0530

    Update Q4.scala

--- Assignment/Q4.scala
+++ Assignment/Q4.scala
@@ -1,33 +1,26 @@
-package Assignment 
+package Assignment
+import Q3.Account
 
 object Q4 extends App
 {
- var bank:List[Account]=List(new Account("Sandaruwan",1201,24760.90),new Account("Gamage",1202,68500.50),new Account("Wikum",1203,-8500.50))
-    def isoverDraft=(x:Account)=>x.balance<0
-    def overDraft=bank.filter(isoverDraft)
-    def balance = (x:Account)=>x.balance
-    var total = bank.map(balance).reduce((a,b)=>(a+b))
-    val interest=(x:Account)=>if (x.balance<0) x.balance=x.balance-x.balance*0.1/100 else x.balance=x.balance+x.balance*0.05/100
-    val a=bank.map(interest)
+    var bank:List[Account] = List()
+    var Print = (a:List[Account])=>(a.map(x => println(x)));
+    val Find=(n:String,b:List[Account])=>b.filter(x=>x.nic.equals(n))
+    val OverDraft = (b:List[Account])=>b.filter(x => (x.balance < 0))
+    val Total  = (b:List[Account])=> b.reduce((x,y) => new Account("",0,"",x.balance + y.balance)).balance
+    val Calculate =(x:Account)=>if(x.balance < 0) x.balance *0.1  else x.balance*0.05
+    val Interest=(b:List[Account])=>b.map(x => x.deposit(Calculate(x)))
     
-    println(bank)
+    var customer1 = new Account("984582675v",4567432,"Sandaruwan",6785.70)
+    var customer2 = new Account("997580435v",523655899,"Dammika",-78)  
+    var customer3 = new Account("968687535v",325423565,"Sumal",190.86)
+    var customer4 = new Account("884587475v",112565985,"Sanjaya",-32.80)
+    var customer5 = new Account("894586592v",112565985,"Janaka",67540.78)
     
-}
-class  Account(id:String,n:Int,b:Double){
-    var nic:String=id
-    var acnumber:Int=n
-    var balance:Double=b
-
-    override def toString="["+nic+":"+acnumber+":"+balance+"]"
-
-    def withdraw(a:Double)
-    {
-        this.balance=this.balance-a
-    }
-
-    def deposit(a:Double)
-    {
-        this.balance=this.balance+a
-    }
-
+    bank = List(customer1,customer2,customer3,customer4,customer5)
+    Interest(bank)
+    println("Account Details That Have Negative Balances : " + OverDraft(bank)) 
+    println("Sum Of All Account Balances : " + Total(bank))
+    println("Updated Account Details")
+    Print(bank)
 }
